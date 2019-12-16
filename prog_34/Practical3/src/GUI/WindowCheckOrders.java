24e44848c128bb0b335428711d111f6ed7f554cd
Jochem24 <j.m.tijburg@student.utwente.nl>
2019-12-16 15:51:45 +0100

    update

--- prog_34/Practical3/src/GUI/WindowCheckOrders.java
+++ prog_34/Practical3/src/GUI/WindowCheckOrders.java
@@ -7,7 +7,7 @@ import Data.*;
 public class WindowCheckOrders extends JFrame {
 	private JPanel buttonsPanel = new JPanel();
 	private JList list = new JList();
-	private JButton b1 = new JButton();
+	private JButton b1 = new JButton("Delete order");
 	private JButton b2 = new JButton("Exit");
 	//private JLabel l1 = new Jlabel();
 	
@@ -15,14 +15,11 @@ public class WindowCheckOrders extends JFrame {
 		super(title);
 		Container myContainer=getContentPane();
 		myContainer.setLayout(new BorderLayout());
+		setSize(800, 600);
 		
 		buttonsPanel.setLayout(new FlowLayout());
 		buttonsPanel.add(b1);
 		buttonsPanel.add(b2);
-		
-		//list.setListData(order);
-		//list.setVisibleRowCount(5);
-		//list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		
 		myContainer.add(buttonsPanel, BorderLayout.SOUTH);
 		myContainer.add(list);
